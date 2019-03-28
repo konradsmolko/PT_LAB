a475a221a175a0656c1c373ad2ef2ad22b5ebcfe
konrad <17689319+konradsmolko@users.noreply.github.com>
2019-03-29 00:17:57 +0100

    trying to make this work, nope.

--- ClientServerSocket/src/ImageProcessingJob.java
+++ ClientServerSocket/src/ImageProcessingJob.java
@@ -1,6 +1,8 @@
 import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
 import javafx.beans.property.DoubleProperty;
-import javafx.concurrent.Task;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -9,16 +11,25 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Stream;
 
-public class ImageProcessingJob {
+class ImageProcessingJob implements Runnable{
+    private File file;
+    private File dir;
+    private DoubleProperty progressProp;
+    private String currentStatus = "Waiting...";
 
+    ImageProcessingJob(File file, File dir, DoubleProperty progressProp) {
+        this.file = file;
+        this.dir = dir;
+        this.progressProp = progressProp;
+    }
 
-    public static void convertToGrayscale(
+    private void convertToGrayscale(
             File sourceFile,
             File destDir,
             DoubleProperty progressProp
     ) {
+        currentStatus = "Processing";
         try {
             BufferedImage original = ImageIO.read(sourceFile);
             BufferedImage grayscale = new BufferedImage(
@@ -38,7 +49,7 @@ public class ImageProcessingJob {
                     grayscale.setRGB(i, j, newPixel);
                 }
                 double progress = (1.0 + i) / original.getWidth();
-                Platform.runLater(() -> progressProp.set(progress));
+                //Platform.runLater(() -> progressProp.set(progress));
             }
 
             Path outputPath = Paths.get(destDir.getAbsolutePath(), sourceFile.getName());
@@ -46,8 +57,76 @@ public class ImageProcessingJob {
             ImageIO.write(grayscale, "jpg", outputPath.toFile());
 
         } catch (IOException e) {
+            currentStatus = "Error";
             System.out.println("Błąd convertToGrayscale.");
         }
+        currentStatus = "Finished";
+    }
 
+    @Override
+    public void run() {
+        convertToGrayscale(file, dir, progressProp);
+    }
+
+    String getFilename() {
+        return file.getName();
+    }
+
+    ObservableValue<String> getStatusProperty() {
+        return new ObservableValue<String>() {
+            @Override
+            public void addListener(ChangeListener<? super String> listener) {
+
+            }
+
+            @Override
+            public void removeListener(ChangeListener<? super String> listener) {
+
+            }
+
+            @Override
+            public String getValue() {
+                return currentStatus;
+            }
+
+            @Override
+            public void addListener(InvalidationListener listener) {
+
+            }
+
+            @Override
+            public void removeListener(InvalidationListener listener) {
+
+            }
+        };
+    }
+
+    ObservableValue<Double> getProgressProperty() {
+        return new ObservableValue<Double>() {
+            @Override
+            public void addListener(ChangeListener<? super Double> listener) {
+
+            }
+
+            @Override
+            public void removeListener(ChangeListener<? super Double> listener) {
+
+            }
+
+            @Override
+            public Double getValue() {
+                return progressProp.getValue();
+            }
+
+            @Override
+            public void addListener(InvalidationListener listener) {
+
+            }
+
+            @Override
+            public void removeListener(InvalidationListener listener) {
+
+            }
+        };
     }
 }
--- ClientServerSocket/src/ImageProcessor.java
+++ ClientServerSocket/src/ImageProcessor.java
@@ -1,5 +1,5 @@
 import javafx.application.Application;
-import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
@@ -12,36 +12,58 @@ import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
 import java.io.File;
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.ResourceBundle;
+import java.util.concurrent.ForkJoinPool;
 
 public class ImageProcessor extends Application {
-    @FXML TableColumn<ImageProcessingJob, String> imageNameColumn;
-    @FXML TableColumn<ImageProcessingJob, Double> progressColumn;
-    @FXML TableColumn<ImageProcessingJob, String> statusColumn;
+    @FXML
+    private TableColumn<ImageProcessingJob, String> imageNameColumn;
+    @FXML
+    private TableColumn<ImageProcessingJob, Double> progressColumn;
+    @FXML
+    private TableColumn<ImageProcessingJob, String> statusColumn;
 
-    private TableView tableView = new TableView();
+    private List<ImageProcessingJob> jobs = new ArrayList<>();
+//    private DoubleProperty progressProp;
+    private ForkJoinPool pool;
+
+    @FXML
+    void processFiles(ActionEvent event) {
+        new Thread(this::backgroundJob).start();
+    }
+
+    private void backgroundJob() {
+        jobs.forEach(pool::execute);
+//        for (ImageProcessingJob job : jobs) pool.execute(job);
+        while (!pool.isTerminated());
+        jobs.clear();
+    }
 
     public static void main(String[] args) { launch(args); }
 
     @FXML
-    public void initialize(URL url, ResourceBundle rb) {
-        imageNameColumn.setCellValueFactory(
-                param -> new SimpleStringProperty(param.getValue().getFile().getName()));
-        statusColumn.setCellValueFactory(
+    private void initialize() {
+        imageNameColumn.setCellValueFactory( // nazwa pliku
+                param -> new SimpleStringProperty(param.getValue().getFilename()));
+        statusColumn.setCellValueFactory( // status przetwarzania
                 param -> param.getValue().getStatusProperty());
-        progressColumn.setCellFactory(
-                ProgressBarTableCell.<ImageProcessingJob>forTableColumn());
-        progressColumn.setCellValueFactory(
-                param -> param.getValue().getProgressProperty().asObject());
+        progressColumn.setCellFactory( // wykorzystanie paska postępu
+                ProgressBarTableCell.forTableColumn());
+        progressColumn.setCellValueFactory( // postęp przetwarzania
+                param -> param.getValue().getProgressProperty());
 
 
     }
 
     @Override
     public void start(Stage primaryStage) {
+        imageNameColumn = new TableColumn<>();
+        progressColumn = new TableColumn<>();
+        statusColumn = new TableColumn<>();
+        initialize();
         primaryStage.setTitle("Przetwarzanie obrazów na czarno-białe");
         primaryStage.setWidth(1000);
         primaryStage.setHeight(750);
@@ -49,20 +71,28 @@ public class ImageProcessor extends Application {
         Scene scene = new Scene(new Group());
 
         final Label label = new Label("Lista plików");
-        Button readButton = new Button("Select files...");
-        Button writeButton = new Button("Choose output directory...");
-        Button runSequential = new Button("Run\n(Sequential)");
-        Button runParallel = new Button("Run\n(Parallel with commonPool)");
-        Button changePool = new Button("Change # of threads in pool\n(default 4)");
-        Button runCustom = new Button("Run\n(Parallel, custom thread pool)");
+        TextField textField = new TextField();
 
+        Button readButton = new Button("Select files and output directory...");
         readButton.setOnAction(event -> selectFiles());
+//        Button writeButton = new Button("Choose output directory...");
+//        writeButton.setOnAction(event -> selectOutDir());
+        Button runSequential = new Button("Run\n(Sequential)");
+        runSequential.setOnAction(event -> runSequential());
+        Button runParallel = new Button("Run\n(Parallel with commonPool)");
+        runParallel.setOnAction(event -> runParallel());
+        Button runCustom = new Button("Run\n(Parallel, custom thread pool)");
+        runCustom.setOnAction(event -> runCustom(textField));
 
 
-        final VBox vbox = new VBox();
+
+        TableView tableView = new TableView();
+        tableView.getColumns().addAll(imageNameColumn, progressColumn, statusColumn);
+        VBox vbox = new VBox();
         vbox.setSpacing(5);
         vbox.setPadding(new Insets(10, 0, 0, 10));
-        vbox.getChildren().addAll(label, tableView);
+        vbox.getChildren().addAll(label, tableView, textField, readButton,
+                runSequential, runParallel, runCustom);
                 //(label, tableView, readButton, writeButton, runSequential, runParallel, changePool, runCustom);
         ((Group) scene.getRoot()).getChildren().addAll(vbox);
 
@@ -75,10 +105,26 @@ public class ImageProcessor extends Application {
         fileChooser.getExtensionFilters().add(
                 new FileChooser.ExtensionFilter("JPG Images", "*.jpg"));
         List<File> selectedFiles = fileChooser.showOpenMultipleDialog(null);
-    }
 
-    private void selectOutDir() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         File chosenDir = directoryChooser.showDialog(null);
+
+        for (File file : selectedFiles) {
+            jobs.add(new ImageProcessingJob(file, chosenDir, null));
+        }
+    }
+
+    private void runCustom(TextField textField) {
+        int count = Integer.parseInt(textField.getText());
+        pool = new ForkJoinPool(count);
+    }
+
+    private void runParallel() {
+        pool = new ForkJoinPool(4);
+    }
+
+    private void runSequential() {
+        pool = new ForkJoinPool(1);
+        processFiles(null);
     }
 }
