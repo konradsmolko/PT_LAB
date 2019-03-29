fad725b778f05b088401d2f1583fdee29b2be6c2
konrad <17689319+konradsmolko@users.noreply.github.com>
2019-03-29 09:41:01 +0100

    init Lab 4

---
+++ ClientServerSocket/src/REST_API.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/*
+    Soczewki
+    -producent
+    -moc
+    -termin ważności
+    -nazwa
+    -cena
+    -l.szt
+    -kolor (opcjonalny)
+
+    Walidacja
+    -dostępny w magazynie
+    -min. 1 produkt
+    -zamówienie >10 PLN
+ */
+
+public class REST_API extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
