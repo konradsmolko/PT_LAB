237c413949f37b215cdee3d8b66f48c1d68a5257
konradsmolko <17689319+konradsmolko@users.noreply.github.com>
2019-06-07 10:59:46 +0200

    finished 1A, 1B

--- Csharp_lab1/Csharp_lab5/App.xaml.cs
+++ Csharp_lab1/Csharp_lab5/App.xaml.cs
@@ -10,29 +10,77 @@ using System.Windows;
 namespace Csharp_lab5
 {
     /// <summary>
-    /// Logika interakcji dla klasy App.xaml
+    /// Logika ulongerakcji dla klasy App.xaml
     /// </summary>
     public partial class App : Application
     {
-        public static int Newton()
+        public static ulong Newton_tasks(ulong K, ulong N)
         {
-            Tuple<int, int> values;
-
-            Thread.Sleep(20); // use this for fibonacci!
-
-            // 1A
-            Task<int> silnia_K = Task.Factory.StartNew<int>(
+            Tuple<ulong, ulong> values = Tuple.Create(K, N);
+            
+            Task<ulong> licznik = Task.Factory.StartNew<ulong>(
                 (obj) =>
                 {
-                    Tuple<int, int> vals = (Tuple<int, int>)obj;
+                    Tuple<ulong, ulong> vals = (Tuple<ulong, ulong>)obj;
+                    K = vals.Item1;
+                    N = vals.Item2;
 
-                    int ret = vals.Item1 / vals.Item2;
-
+                    ulong ret = 1;
+                    for (ulong i = N; i > N - K; i--)
+                        ret *= i;
+                    Thread.Sleep(20);
                     return ret;
-                },
-                100
-            );
+                }, values);
+
+            Task<ulong> mianownik = Task.Factory.StartNew<ulong>(
+                (obj) =>
+                {
+                    ulong ret = 1;
+                    for (ulong i = K; i > 0; i--)
+                    {
+                        ret *= i;
+                    }
+                    Thread.Sleep(20);
+                    return ret;
+                }, K);
+
+            // Accessing the property's get accessor blocks the calling thread until the asynchronous
+            // operation is complete; it is equivalent to calling the Wait method.
+            ulong l = licznik.Result;
+            ulong m = mianownik.Result;
+            return l/m;
+        }
+
+        public static ulong silnia(ulong start, ulong end)
+        {
+            ulong ret = 1;
+            for (ulong i = end; i > end - start; i--)
+                ret *= i;
+            Thread.Sleep(20);
+            return ret;
+        }
+
+        public static ulong Newton_delegates(ulong K, ulong N)
+        {
+            Func<ulong, ulong, ulong> op = silnia;
+            IAsyncResult result_licznik;
+            IAsyncResult result_mianownik;
+            result_licznik = op.BeginInvoke(K, N, null, null);
+            result_mianownik = op.BeginInvoke(K, K, null, null);
+
+            ulong licznik = op.EndInvoke(result_licznik);
+            ulong mianownik = op.EndInvoke(result_mianownik);
+
+            return licznik/mianownik;
+        }
+
+        public static ulong Newton_async(ulong K, ulong N)
+        {
+
+
             return 0;
         }
+        //Thread.Sleep(20); // use this for fibonacci!
+
     }
 }
--- Csharp_lab1/Csharp_lab5/MainWindow.xaml.cs
+++ Csharp_lab1/Csharp_lab5/MainWindow.xaml.cs
@@ -16,18 +16,57 @@ using System.Windows.Shapes;
 namespace Csharp_lab5
 {
     /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
+    /// Logika ulongerakcji dla klasy MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            // this.DNS_return.Text = "Kappa";
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Btn_newton_tasks(object sender, RoutedEventArgs e)
         {
-            int i = App.Newton();
+            ulong ret;
+            ulong.TryParse(this.Newton_K.Text, out ulong K);
+            ulong.TryParse(this.Newton_N.Text, out ulong N);
+            if (K > N) return;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            ret = App.Newton_tasks(K, N);
+            watch.Stop();
+            this.time_taken.Content = ((double)watch.ElapsedMilliseconds / 1000).ToString() + " seconds";
+
+            Tasks_amount.Text = ret.ToString();
+        }
+
+        private void Btn_newton_delegates(object sender, RoutedEventArgs e)
+        {
+            
+            ulong ret;
+            ulong.TryParse(this.Newton_K.Text, out ulong K);
+            ulong.TryParse(this.Newton_N.Text, out ulong N);
+            if (K > N) return;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            ret = App.Newton_delegates(K, N);
+            watch.Stop();
+            this.time_taken.Content = ((double)watch.ElapsedMilliseconds / 1000).ToString() + " seconds";
+
+            Delegates_amount.Text = ret.ToString();
+        }
+
+        private void Btn_newton_async(object sender, RoutedEventArgs e)
+        {
+            ulong ret;
+            ulong.TryParse(this.Newton_K.Text, out ulong K);
+            ulong.TryParse(this.Newton_N.Text, out ulong N);
+            if (K > N) return;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            ret = App.Newton_async(K, N);
+            watch.Stop();
+            this.time_taken.Content = ((double)watch.ElapsedMilliseconds / 1000).ToString() + " seconds";
+
+            Async_await_amount.Text = ret.ToString();
         }
     }
 }
